e8c69c1f45dd4bdb246d54c815d98f7ccd37ed49
aniyahmb99 <aniyah_mb@yahoo.com>
2022-07-04 13:56:34 -0400

    finished workout card

---
+++ builtnotbought/src/components/WorkoutCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
+
+const WorkoutCard = ({ workout }) => {
+  return (
+    <Link className="workout-card" to={`/workout/${workout.id}`}> 
+    <img src={workout.gifUrl} alt={workout.name} Loading="lazy" />
+    <Stack direction="row">
+        <Button sx={{ ml: '21px', color: '#fff', background: '#ffa9a9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
+            {workout.bodyPart}
+        </Button>
+        <Button sx={{ ml: '21px', color: #fff, background: '#fcc757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
+        {workout.target}
+        </Button>
+    </Stack>
+    <Typography mL='21px' color='#000' fontWeight="bold" mt="11px" pb="10px" textTransform="capitalize" fontSize="22px">
+        {workout.name}
+    </Typography>
+      </Link>
+  );
+};
+
+export default WorkoutCard
