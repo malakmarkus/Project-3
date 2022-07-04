ae68a7bf6616e5c65fd92967fbe84ced392a17a1
aniyahmb99 <aniyah_mb@yahoo.com>
2022-07-04 15:35:32 -0400

    refractored code a little

--- builtnotbought/src/components/Footer.js
+++ builtnotbought/src/components/Footer.js
@@ -2,17 +2,27 @@ import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import Logo from "../assets/images/Logo-1.png";
 
-const Footer = () => {
-  return (
-    <Box mt="80px" bgcolor="#fff3f4">
-      <Stack gap="40px" alignitems="center" px="40px" pt="24px">
-        <img src={Logo} alt="logo" width="200px" height="40px" />
-        <Typography variant="h5" pb="40px" mt="20px">
-          Made with ❤️ by Build-Not-Bought© 💪
-        </Typography>
-      </Stack>
-    </Box>
-  );
-};
+const Footer = () => (
+  <Box mt="80px" bgcolor="#fff3f4">
+    <Stack
+      gap="40px"
+      sx={{ alignitems: "center" }}
+      flexWrap="wrap"
+      px="40px"
+      pt="24px"
+    >
+      <img src={Logo} alt="logo" style={{ width: "200px", height: "41px" }} />
+    </Stack>
+    <Typography
+      variant="h5"
+      sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+      mt="41px"
+      textAlign="center"
+      pb="40px"
+    >
+      Made with ❤️ by Build-Not-Bought© 💪
+    </Typography>
+  </Box>
+);
 
 export default Footer;
