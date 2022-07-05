3044e8bd0ae9674eab3f01f564ced932a8009cd1
domfinch <finch.163@buckeyemail.osu.edu>
2022-07-05 19:22:39 -0400

    main

---
+++ builtnotbought/src/components/bodyParts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Stack, Typography } from '@mui/material';
+
+import icon from '../assets/icons/gym.png'
+
+const BodyPart = () => {
+    return (
+        <div>BodyPart</div>
+    )
+}
+
+
+export default BodyPart
