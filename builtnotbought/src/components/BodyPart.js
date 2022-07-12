9ac655124abeef699b6a967231e7c8c0cad97f78
domfinch <finch.163@buckeyemail.osu.edu>
2022-07-12 18:57:23 -0400

    error fix

--- builtnotbought/src/components/BodyPart.js
+++ builtnotbought/src/components/BodyPart.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/body-part.png';
 
-const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+const bodyPart = ({ item, setbodyPart, bodyPart }) => (
   <Stack
     type="button"
     alignItems="center"
     justifyContent="center"
     className="bodyPart-card"
-    sx={bodyPart === item ? { borderTop: '4px solid skyBlue', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
+    sx={bodyPart === item ? { borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
     onClick={() => {
-      setBodyPart(item);
+      setbodyPart(item);
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
     }}
   >
@@ -19,4 +19,4 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
   </Stack>
 );
 
-export default BodyPart;
+export default bodyPart;
