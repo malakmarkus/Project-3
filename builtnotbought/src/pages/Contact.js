1be6fc333ad4ad629127799c95abe3c851dff495
Your Name <daquan.jordan22@gmail.com>
2022-07-12 13:01:17 -0400

    responsive form

--- builtnotbought/src/pages/Contact.js
+++ builtnotbought/src/pages/Contact.js
@@ -4,38 +4,21 @@ import React, { useState } from 'react';
 import { validateEmail } from '../utils/helpers';
 
 function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const [] = useState({ name: '', email: '', message: '' });
 
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
+  const [] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
+  <Stack
+  
+    direction="row "
+    justifyContent="space-around"
+    sx={
+      gap: { sm: "123px", xs: "40px" },
+      mt: { sm: "32px", xs: "20px" },
+      justifyContent: "none",
     }
-  };
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
-  };
+
 
   return (
     <div class="">
